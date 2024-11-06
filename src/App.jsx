af6c8178a384a6730c9a529b1fa74354486ebe42
Leandro Garcia Tosi <leandrotosi07@gmail.com>
2024-11-06 00:21:13 -0300

    creacion de componentes Item y estilo basico de tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/NavBar'
 import ItemCount from './components/ItemCount/ItemCount'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 
 function App() {
 
@@ -11,11 +12,7 @@ function App() {
       <h1>  Premium Pet Shop </h1>
       <NavBar />
       <ItemListContainer greeting={'bienvenidos'} />
-      <ItemCount 
-      initial={1} 
-      stock={10} 
-      onAdd={(quantity) => console.log('cantidad agregada:',quantity)}
-      />
+      <ItemDetailContainer/>
     </div>
   )
 }
--- src/mocks/asyncMock.jsx
+++ src/mocks/asyncMock.jsx
@@ -1,46 +1,54 @@
 const products = [
     {
-        id:'1',
-        name:'Raton de juguetes para gatos',
+        id: '1',
+        name: 'Raton de juguetes para gatos',
         price: 500,
-        category:'Juguetes',
-        img:'https://iili.io/2z3SdfS.jpg',
-        stock:500,
-        description:'Ratón de juguete con cola de cuerda para gatos, diseñado para simular los movimientos de una presa.'
+        category: 'Juguetes',
+        img: 'https://iili.io/2z3SdfS.jpg',
+        stock: 500,
+        description: 'Ratón de juguete con cola de cuerda para gatos, diseñado para simular los movimientos de una presa.'
     },
     {
-        id:'2',
-        name:'Pelota interactiva para perros',
+        id: '2',
+        name: 'Pelota interactiva para perros',
         price: 1000,
-        category:'Juguetes',
-        img:'https://iili.io/2z38QUX.jpg',
-        stock:500,
-        description:'Pelota resistente para perros, con diseño de goma que bota en diferentes direcciones, manteniéndolos entretenidos.'
+        category: 'Juguetes',
+        img: 'https://iili.io/2z38QUX.jpg',
+        stock: 500,
+        description: 'Pelota resistente para perros, con diseño de goma que bota en diferentes direcciones, manteniéndolos entretenidos.'
     },
     {
-        id:'3',
-        name:'Antipulgas y garrapaticida para perros (pipeta)',
+        id: '3',
+        name: 'Antipulgas y garrapaticida para perros (pipeta)',
         price: 2000,
-        category:'Medicamentos',
-        img:'https://iili.io/2z388Hg.jpg',
-        stock:500,
-        description:' Pipeta mensual antipulgas y garrapaticida para perros, eficaz durante 30 días. Apto para perros medianos y grandes.'
+        category: 'Medicamentos',
+        img: 'https://iili.io/2z388Hg.jpg',
+        stock: 500,
+        description: ' Pipeta mensual antipulgas y garrapaticida para perros, eficaz durante 30 días. Apto para perros medianos y grandes.'
     },
     {
-        id:'4',
-        name:'Alimento lata 400g',
+        id: '4',
+        name: 'Alimento lata 400g',
         price: 10000,
-        category:'Alimento',
-        img:'https://iili.io/2z3S6mb.webp',
-        stock:500,
-        description:'Lata de comida húmeda para perros, rica en carne y sin colorantes ni conservantes.'
+        category: 'Alimento',
+        img: 'https://iili.io/2z3S6mb.webp',
+        stock: 500,
+        description: 'Lata de comida húmeda para perros, rica en carne y sin colorantes ni conservantes.'
     }
 ]
 
 export const getProducts = () => {
-    return new Promise ((resolve) => {
-        setTimeout(()=>{
+    return new Promise((resolve) => {
+        setTimeout(() => {
             resolve(products)
-        },500)
+        }, 500)
+    })
+}
+
+export const getProductById = (productId) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(products.find(prod => prod.id === productId))
+        },500);
     })
 }
