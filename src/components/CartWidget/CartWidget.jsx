b15d42216b17f6d2c5fd7089ac9478799f844934
Leandro Garcia Tosi <leandrotosi07@gmail.com>
2024-11-07 00:10:13 -0300

    styles fixed v3

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,25 +1,19 @@
 import { CartIcon } from '../Icons/Icons'
 import './CartWidget.css'
 import { useCartContext } from "../../context/CartContext"
-import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
-
-
-function CartWidget (){
-
+function CartWidget() {
     const { qtyItems } = useCartContext()
-
 
     return (
         <section className="Cart">
-                <div className="cart-icon-container">
-
-                    <Link to="/cart">
-                    <CartIcon/>
-                    </Link>
-                    <span className="qtyItems">{qtyItems}</span>
-                </div>
+            <div className="cart-icon-container">
+                <Link to="/cart">
+                    <CartIcon />
+                    {qtyItems > 0 && <span className="qtyItems">{qtyItems}</span>}
+                </Link>
+            </div>
         </section>
     );
 }
