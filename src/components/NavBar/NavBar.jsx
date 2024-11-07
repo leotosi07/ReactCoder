6fe60fca06abce262e671b1e92336160e7da19d7
Leandro Garcia Tosi <leandrotosi07@gmail.com>
2024-11-07 00:53:06 -0300

    addItem style changed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,20 @@
-import { NavLink,Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import CartWidget from '../CartWidget/CartWidget'
 import './NavBar.css'
+import icon from '../../assets/icon.jpg';
+
+
 
 const NavBar = () => {
     return (
         <nav className='NavBar'>
             <Link to='/'>
-            <h3>Premium Pet Shop</h3>
+                    <h3>Premium Pet Shop</h3>
             </Link>
             <div>
-                <NavLink to={`/category/Juguetes`} className={({isActive}) => isActive ? 'ActiveOption':'Option'}>Juguetes</NavLink>
-                <NavLink to={`/category/Medicamentos`} className={({isActive}) => isActive ? 'ActiveOption':'Option'}>Medicamentos</NavLink>
-                <NavLink to={`/category/Alimento`} className={({isActive}) => isActive ? 'ActiveOption':'Option'}>Alimento</NavLink>
+                <NavLink to={`/category/Juguetes`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Juguetes</NavLink>
+                <NavLink to={`/category/Medicamentos`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Medicamentos</NavLink>
+                <NavLink to={`/category/Alimento`} className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}>Alimento</NavLink>
             </div>
             <CartWidget />
         </nav>
